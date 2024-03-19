9f447c003f92f96eb23548e0f0966ca0dd6853e6
Dmitry Tinkov <mrfredyon@gmail.com>
2024-03-19 22:21:33 +0300

    xzc

---
+++ src/tests/service/api.service.test.ts
@@ -0,0 +1,9 @@
+import { createUser } from "../../service/api.service";
+import * as repository from "../../repository/api.repository";
+
+
+describe('create user', ()=>{
+    test('correct', async ()=>{
+        const mock = jest.spyOn()
+    })
+})
